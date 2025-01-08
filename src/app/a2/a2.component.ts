3ef96b1c5b4898bcb1152151c7cc3663075b60be
Daniel Epstein <epstein@ics.uci.edu>
2025-01-08 14:32:37 -0800

    Post office hours time & A2

--- src/app/a2/a2.component.ts
+++ src/app/a2/a2.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import moment from 'moment';
 
 @Component({
   selector: 'app-a2',
@@ -7,5 +9,23 @@ import { Component } from '@angular/core';
   styleUrl: './a2.component.css'
 })
 export class A2Component {
+  assignment:any;
+  
+    constructor(private http:HttpClient) {
+      this.http.get('calendar.json').subscribe(calendar => {
+        this.parseAssignment(calendar as {}, 'A2');
+      });
+    }
+  
+    parseAssignment(calendar:any, assignment:string) {
+      let events:any[] = calendar['events'];
+  
+      //Filter to this assignment
+      this.assignment = events.filter(e => e['type'] == 'assignment' && e['title'] == assignment)[0];
+      
+      //Set due date
+    let due = moment(this.assignment['date'] + " " + calendar['defaults'].assignment.due);
+      this.assignment['due'] = due.format('dddd, MMMM Do, h:mma');
+    }
 
 }
